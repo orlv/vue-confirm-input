f4d0043a440dbe732effa2b19f90247ff9747e2a
orlv <oleg.orlv@gmail.com>
2022-03-15 11:49:27 +0400

    Update styles

--- vue-confirm-input.vue
+++ vue-confirm-input.vue
@@ -1,19 +1,23 @@
 <template>
     <span>
-        <span class="text-button p-0" :class="{'text-button-red': !busy }" @click="click">{{ buttonText }}</span>
+        <span class="show-button" :class="{'show-button-idle': !busy }" @click.stop="click">{{ buttonText }}</span>
         <span class="container" v-if="busy" @click="cancel">
             <span @click.stop class="vue-confirm-input-main">
                 <span>{{ title }}</span>
-                <input v-model="inputValue" type="text" class="ml-1" :title="title" autofocus ref="confirmInput"
+                <input v-model.trim="inputValue"
+                       type="text"
+                       class="confirm-input"
+                       :title="title"
+                       autofocus
+                       ref="confirmInput"
                        @keydown.enter="callConfirm"/>
-                <span class="text-button text-button-green" @click="callConfirm">{{ confirmText }}</span>
+                <span class="confirm-button" @click.stop="callConfirm">{{ confirmText }}</span>
             </span>
         </span>
     </span>
 </template>
 
 <script>
-import 'rlv-styles/styles.css'
 
 export default {
   data () {
@@ -68,22 +72,61 @@ export default {
 </script>
 
 <style scoped>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: rgba(249, 249, 249, 0.89);
-    }
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(249, 249, 249, 0.89);
+}
 
-    .container > span {
-        background-color: #ffffff;
-        padding: 1em;
-        border-radius: 0.3em;
-        border: 1px solid #e2e2e2;
-    }
+.container > span {
+    background-color: #ffffff;
+    padding: 1em;
+    border-radius: 0.3em;
+    border: 1px solid #e2e2e2;
+}
+
+.show-button {
+    padding: 0;
+    cursor: pointer;
+    font-weight: 700;
+    text-align: center;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+.show-button-idle {
+    color: #e0412e;
+}
+
+.show-button-idle:hover {
+    color: #d8422f;
+}
+
+.confirm-button {
+    padding: 0 0.6em;
+    cursor: pointer;
+    font-weight: 700;
+    text-align: center;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    color: #70ab6c;
+}
+
+.confirm-button:hover {
+    color: #409a39;
+}
+
+.confirm-input {
+    margin-left: 1em;
+}
 </style>
